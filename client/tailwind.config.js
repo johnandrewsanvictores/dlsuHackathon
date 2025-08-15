4777f0d19bb2ffbcc8fac7ffe333aae2602ef2c6
Nicko Balmes <nckoblms@gmail.com>
2025-08-15 22:09:20 +0800

    Homepage + styles var

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        brand: {
+          honey: {
+            DEFAULT: "#FFC93C",
+            50: "#FFF3C6",
+            600: "#F5BE28",
+          },
+          bee: "#1C1C1C",
+          soft: "#FDFDFD",
+          amber: "#FFB100",
+          gray: "#A9A9A9",
+        },
+      },
       fontSize: {
         h1: "3.815rem",
         h2: "3.052rem",
@@ -15,7 +27,10 @@ module.exports = {
         p: "1rem",
         small: "0.8rem",
       },
-      fontFamily: {}
-    }
-  }
+      fontFamily: {
+        poppins: ["Poppins", "ui-sans-serif", "system-ui", "sans-serif"],
+        roboto: ["Roboto", "ui-sans-serif", "system-ui", "sans-serif"],
+      },
+    },
+  },
 };
